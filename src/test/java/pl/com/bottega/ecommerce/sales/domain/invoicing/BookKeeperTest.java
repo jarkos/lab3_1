692ed6a79a5d82eaa936375a15905a46f347d454
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-03-28 19:53:52 +0100

    Added Test case 2

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.hamcrest.CoreMatchers.is;
 
-
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -16,6 +15,9 @@ import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import org.mockito.Mockito;
+import org.mockito.verification.VerificationMode;
 
 public class BookKeeperTest 
 {
@@ -51,6 +53,40 @@ public class BookKeeperTest
 		
 	}
 	
-	
+	@Test
+	public void giveInvoiceRequestWithTwoPosition_shouldInvokeCalculateTaxMethodTwice() 
+	{
+
+		InvoiceFactory invoiceFactory = mock(InvoiceFactory.class);
+		TaxPolicy taxPolicy = mock(TaxPolicy.class);
+		
+		BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+		Id clientId = new Id("K1");
+		ClientData clientData = new ClientData(Id.generate(), "Data");
+		when(invoiceFactory.create(clientData)).thenReturn(new Invoice(clientId, clientData));
+		
+		ProductType productType = ProductType.STANDARD;
+		BigDecimal denomination = new BigDecimal(10);
+		Money money = new Money(denomination);
+		when(taxPolicy.calculateTax(productType, money)).thenReturn(new Tax(money, "opis"));
+		//product1
+		Id productId1 = new Id("P1");
+		Date date1 = new Date(2014);
+		ProductData productData1 = new ProductData(productId1,money,"phone1",productType,date1);
+		//product2
+		Id productId2 = new Id("P2");
+		Date date2 = new Date(2014);
+		ProductData productData2 = new ProductData(productId2,money,"phone2",productType,date2);
+		
+		RequestItem requestItem1 = new RequestItem(productData1, 1,money);
+		RequestItem requestItem2 = new RequestItem(productData2, 1,money);
+		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+		invoiceRequest.add(requestItem1);
+		invoiceRequest.add(requestItem2);
+		
+		Invoice resultInvoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+		Mockito.verify(taxPolicy, Mockito.times(2)).calculateTax(productType, money);
+		
+	}
 	
 }
