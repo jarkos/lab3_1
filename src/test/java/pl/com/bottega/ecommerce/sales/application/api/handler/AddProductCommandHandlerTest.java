43fa7f0f575054749ff193512f23194e7b6d4664
Jaroslaw Kostrzewa <180216@edu.p.lodz.pl>
2015-04-15 19:24:55 +0200

    Addedc class AddProductCommandHandlerTest

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -0,0 +1,14 @@
+package pl.com.bottega.ecommerce.sales.application.api.handler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddProductCommandHandlerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
